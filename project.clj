65f707916c0b2c796ef8fee60f1d57520edb3cdd
YoungJoo Lee <youngker@gmail.com>
2016-09-07 17:07:26 +0900

    add dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [tesser.core "1.0.1"]
+                 [tesser.math "1.0.1"]
+                 [rewrite-clj "0.5.2"]
+                 [iota "1.1.3"]])
--- src/charon/core.clj
+++ src/charon/core.clj
@@ -1,6 +1,38 @@
-(ns charon.core)
+(ns charon.core
+  (:require
+   [clojure.core.reducers :as r]
+   [clojure.edn :as edn]
+   [clojure.java.io :refer [reader writer file]]
+   [clojure.string :as s]
+   [iota]
+   [rewrite-clj.node :as n]
+   [rewrite-clj.parser :as p]
+   [rewrite-clj.zip :as z]
+   [tesser.core :as t]
+   [tesser.math :as m])
+  (:import
+   (java.io PushbackReader)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn trigrams [s]
+  (partition 3 1 (s/triml s)))
+
+(defn clean [s]
+  (map #(keyword (s/replace (s/join %) #" " "_")) s))
+
+(defn write [f]
+  (with-open [w (writer "index.edn")]
+    (let [s (parse f)]
+      (.write w (prn-str (into (sorted-map) (frequencies (flatten s))))))))
+
+(defn parse [f]
+  (->> (iota/seq f)
+       (r/filter identity)
+       (r/map trigrams)
+       (r/map clean)
+       (into [])))
+
+(defn read-dir [d]
+  (let [dir (file d)
+        files (filter #(.isFile %) (file-seq dir))]
+    (doseq [file files]
+      (add file))))
