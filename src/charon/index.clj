062268be3cfe76867c4f49451d0ff45c858a6b8d
YoungJoo Lee <youngker@gmail.com>
2016-09-21 17:01:13 +0900

    cleanup code

--- src/charon/index.clj
+++ src/charon/index.clj
@@ -7,7 +7,7 @@
   (:import
    (java.io File)))
 
-(defn file-seq-ignore
+(defn- file-seq-ignore
   [dir exceptions]
   (let [exceptions (set exceptions)]
     (tree-seq
@@ -25,13 +25,13 @@
 (defn trigrams [s]
   (partition 3 1 s))
 
-(defn- get-trigrams [file]
-  (println (.getPath ^File file))
+(defn- read-file [#^File file]
+  (println (.getPath file))
   (mapcat #(trigrams (s/triml %)) (s/split-lines (slurp file :encoding "UTF-8"))))
 
-(defn- process [file]
+(defn- get-trigrams [file]
   (map #(hash-map (s/join %) #{(first file)})
-       (get-trigrams (second file))))
+       (read-file (second file))))
 
 (defn- write-index [m d]
   (with-open [w (writer (str d "/.charon/index.edn") :encoding "UTF-8")]
@@ -41,5 +41,6 @@
   (let [dir (:index opts)
         files (read-dir dir)
         file-list (zipmap (map first files) (map #(.getPath ^File (second %)) files))
-        result (apply merge-with set/union (mapcat process files))]
-    (write-index {:file-list file-list :trigrams result} dir)))
+        trigrams (apply merge-with set/union (apply concat (pmap get-trigrams files)))]
+    (write-index {:file-list file-list :trigrams trigrams} dir)
+    (shutdown-agents)))
