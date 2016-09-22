6f21507e939e6e8828630c7dc7f496624da16d20
YoungJoo Lee <youngker@gmail.com>
2016-09-22 17:21:51 +0900

    add validutf-8

--- src/charon/index.clj
+++ src/charon/index.clj
@@ -25,13 +25,32 @@
 (defn trigrams [s]
   (partition 3 1 s))
 
+(defn- utf8? [c1 c2]
+  (cond
+    (< c1 0x80) (or (< c2 0x80) (and (<= 0xc0 c2) (< c2 0xf8)))
+    (< c1 0xc0) (< c2 0xf8)
+    (< c1 0xf8) (and (<= 0x80 c2) (< c2 0xc0))
+    :else false))
+
+(defn- check-trigram [s]
+  (if (some false? (map #(utf8? (bit-and (bit-shift-right (int %) 8) 0xff)
+                                (bit-and (int %) 0xff)) s))
+    nil
+    (s/join s)))
+
 (defn- read-file [#^File file]
   (println (.getPath file))
-  (mapcat #(trigrams (s/triml %)) (s/split-lines (slurp file :encoding "UTF-8"))))
+  (apply concat (reduce (fn [v l]
+                          (let [t (trigrams (s/triml l))
+                                s (map #(check-trigram %) t)]
+                            (if (some nil? s)
+                              (reduced nil)
+                              (do () (conj v s)))))
+                        []
+                        (s/split-lines (slurp file :encoding "UTF-8")))))
 
 (defn- get-trigrams [file]
-  (map #(hash-map (s/join %) #{(first file)})
-       (read-file (second file))))
+  (zipmap (read-file (second file)) (repeat #{(first file)})))
 
 (defn- write-index [m d]
   (with-open [w (writer (str d "/.charon/index.edn") :encoding "UTF-8")]
@@ -41,6 +60,6 @@
   (let [dir (:index opts)
         files (read-dir dir)
         file-list (zipmap (map first files) (map #(.getPath ^File (second %)) files))
-        trigrams (apply merge-with set/union (apply concat (pmap get-trigrams files)))]
-    (write-index {:file-list file-list :trigrams trigrams} dir)
+        trigrams (apply merge-with set/union (pmap get-trigrams files))]
+    (time (write-index {:file-list file-list :trigrams trigrams} dir))
     (shutdown-agents)))
